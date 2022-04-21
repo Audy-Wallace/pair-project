a8142e4cd6c01cbb195e72e68b5c7212a424ebd2
Audy Wallace <hwksin7@gmail.com>
2022-04-21 11:53:54 +0700

    new commit

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const port = 3000
 const Controller = require('./controllers/index')
 
 app.use(express.urlencoded({extended:true}))
+app.use("/home", require("./routes/home"))
+
 app.set('view engine','ejs')
 app.get('/', Controller.registerForm)
+app.post('/', Controller.postRegister)
 app.get('/login', Controller.loginForm)
+app.post('/login', Controller.cekLogin)
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
