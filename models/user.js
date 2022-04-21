70cd5b0a4d22e0c111f61efe7a1ba90e1551868b
raviharyo <raffiiphone99@gmail.com>
2022-04-21 11:41:07 +0700

    Adding bcrypt on login

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 'use strict';
+const bcrypt = require('bcryptjs')
 const {
   Model
 } = require('sequelize');
@@ -11,17 +12,33 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      User.hasOne(models.UserIdentity, {foreignKey: "UserId"})
+      User.hasOne(models.UserIdentity, { foreignKey: "UserId" })
     }
   }
   User.init({
     email: DataTypes.STRING,
     password: DataTypes.STRING,
-    role: DataTypes.INTEGER,
-    UserId: DataTypes.INTEGER
+    role: DataTypes.INTEGER
+
   }, {
     sequelize,
     modelName: 'User',
+    hooks: {
+      beforeCreate(instance, options) {
+        let kata = ""
+        var salt = bcrypt.genSaltSync(8);
+        var hash = bcrypt.hashSync(instance.password, salt);
+        instance.password = hash
+        kata = instance.email.split("@")[1]
+        kata = kata.split(".")[0]
+        if (kata == 'admin' || kata == 'Admin') {
+          instance.role = 1
+        } else {
+          instance.role = 2
+        }
+      }
+
+    }
   });
   return User;
 };
