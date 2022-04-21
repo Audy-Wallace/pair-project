32a5f46f83427099ff9c7f80532fca8889ba6f36
raviharyo <raffiiphone99@gmail.com>
2022-04-21 14:38:29 +0700

    giving err info to ejs

--- controllers/index.js
+++ controllers/index.js
@@ -1,62 +1,72 @@
-const {UserIdentity, User} = require('../models')
+const { UserIdentity, User } = require('../models')
 const bcrypt = require('bcryptjs')
 
 class Controller {
 
-    static registerForm(req,res){
+    static registerForm(req, res) {
         res.render('register.ejs')
     }
-    static loginForm(req,res){
-        res.render('login.ejs')
+    static loginForm(req, res) {
+        let errors = req.query.errors
+        res.render('login.ejs', {errors})
     }
 
-    static postRegister(req,res){
+    static postRegister(req, res) {
         // console.log(req.body);
-        const {email,password,dateOfBirth,gender,firstName,lastName} = req.body
+        const { email, password, dateOfBirth, gender, firstName, lastName } = req.body
         User.create({
             email: email,
             password: password
         })
-        .then(data=>{
-            // console.log(data);
-            return UserIdentity.create({
-                firstName: firstName,
-                lastName: lastName,
-                dateOfBirth: dateOfBirth,
-                gender: gender,
-                UserId: data.id
+            .then(data => {
+                // console.log(data);
+                return UserIdentity.create({
+                    firstName: firstName,
+                    lastName: lastName,
+                    dateOfBirth: dateOfBirth,
+                    gender: gender,
+                    UserId: data.id
+                })
+                    .then(data2 => {
+                        res.redirect('/login')
+                    })
             })
-            .then(data2=>{
-                res.redirect('/login')
+            .catch(err => {
+                console.log(err);
+                res.send(err)
             })
-        })
-        .catch(err=>{
-            console.log(err);
-            res.send(err)
-        })
     }
 
-    static cekLogin(req,res){       
-        const {email, password} = req.body
+    static cekLogin(req, res) {
+        const { email, password } = req.body
         User.findOne({
-            where:{
+            where: {
                 email: email
             }
         })
-        .then(user=>{
-            if(user){
-                const isValidPassword = bcrypt.compareSync(password, user.password)
-                if(isValidPassword) {
-                    return res.send('email dan password muasok')
+            .then(user => {
+                if (user) {
+                    const isValidPassword = bcrypt.compareSync(password, user.password)
+                    if (isValidPassword) {   
+                        // Case saat berhasil login
+                        req.session.iduser = user.id //set session 
+                        req.session.roleuser = user.role //set role
+                        res.redirect('/home')
+                    } else {
+                        let errors = 'Invalid username/password'
+                        return res.redirect(`/login?errors=${errors}`)
+                        // return res.send('email dan password gak cocok boz')
+                    }
                 } else {
-                    return res.send('email dan password gak cocok boz')
+                    let errors = 'Invalid username/password'
+                    return res.redirect(`/login?errors=${errors}`)
+                //   return  res.send('E-mail belom terdaftar')
                 }
-            }
-        })
-        .catch(err=>{
-            console.log(err);
-            res.send(err)
-        })
+            })
+            .catch(err => {
+                console.log(err);
+                res.send(err)
+            })
     }
 
 }
--- routes/home.js
+++ routes/home.js
@@ -4,6 +4,15 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", HomeController.home);
+    router.use((req, res, next) => {
+        console.log(req.session)
+        if(!req.session.iduser){
+            let errors = 'Login dulu ya'
+            res.redirect(`/login?errors=${errors}`)
+        } else {
+            next()
+        }
+      })
 router.get("/courses", HomeController.courses);
 
 module.exports = router;
